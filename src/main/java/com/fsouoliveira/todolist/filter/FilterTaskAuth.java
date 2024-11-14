aa0a17049610de2d6afb3c11a07cea3ab625f0ee
ithub.com/fsouoliveira <f.sou.oli99@gmail.com>
2024-11-14 18:04:55 -0300

    ContinuingFilterImplementationProjectToDoList

--- src/main/java/com/fsouoliveira/todolist/filter/FilterTaskAuth.java
+++ src/main/java/com/fsouoliveira/todolist/filter/FilterTaskAuth.java
@@ -1,8 +1,11 @@
 package com.fsouoliveira.todolist.filter;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import com.fsouoliveira.todolist.user.IUserRepository;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -12,23 +15,46 @@ import java.util.Base64;
 @Component
 public class FilterTaskAuth extends OncePerRequestFilter {
 
+    @Autowired
+    private IUserRepository userRepository;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-          var authorization = request.getHeader("Authorization");
 
-        var authEncoded = authorization.substring("Basic".length()).trim();
+        var servletPath = request.getServletPath();
 
-        byte[] authDecoded = Base64.getDecoder().decode(authEncoded);
+        if (servletPath.equals("/tasks/")) {
 
-        var authString = new String(authDecoded);
+            var authorization = request.getHeader("Authorization");
 
-        String[] credentials = authString.split(":");
-        String username = credentials[0];
-        String password = credentials[1];
+            var authEncoded = authorization.substring("Basic".length()).trim();
 
-        System.out.println("Authorization");
-        System.out.println(authString);
-        System.out.println(username);
-        System.out.println(password);
+            byte[] authDecoded = Base64.getDecoder().decode(authEncoded);
+
+            var authString = new String(authDecoded);
+
+            String[] credentials = authString.split(":");
+            String username = credentials[0];
+            String password = credentials[1];
+
+            var user = this.userRepository.findByUsername(username);
+            if (user == null) {
+                response.sendError(401);
+            } else {
+
+                var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+                if (passwordVerify.verified) {
+                    request.setAttribute("idUser", user.getId());
+                    filterChain.doFilter(request, response);
+                } else {
+                    response.sendError(401);
+                }
+
+
+            }
+
+        }else {
+            filterChain.doFilter(request, response);
+        }
     }
 }
--- src/main/java/com/fsouoliveira/todolist/task/TaskController.java
+++ src/main/java/com/fsouoliveira/todolist/task/TaskController.java
@@ -1,10 +1,15 @@
 package com.fsouoliveira.todolist.task;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/tasks")
@@ -14,8 +19,22 @@ public class TaskController {
     private ITaskRepository taskRepository;
 
     @PostMapping("/")
-    public TaskModel create(@RequestBody TaskModel taskModel) {
-        System.out.println("Arrived in the controller");
-        return this.taskRepository.save(taskModel);
+    public ResponseEntity create(@RequestBody TaskModel taskModel) {
+
+        if (taskModel.getIdUser() == null) {
+            taskModel.setIdUser(UUID.randomUUID());
+        }
+
+        var currentDate = LocalDateTime.now();
+        if (currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAT())) {
+            return  ResponseEntity.status(HttpStatus.BAD_REQUEST).body("The start / termination date must be in the future.");
+        }
+
+        if (taskModel.getStartAt().isAfter(taskModel.getEndAT())) {
+            return  ResponseEntity.status(HttpStatus.BAD_REQUEST).body("The start date must precede the end date.");
+        }
+
+        var task = this.taskRepository.save(taskModel);
+        return ResponseEntity.status(HttpStatus.OK).body(task);
     }
 }
