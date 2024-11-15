634ba6299ff09cbdd00e436d870a831742b91e7e
ithub.com/fsouoliveira <f.sou.oli99@gmail.com>
2024-11-15 20:10:50 -0300

    UtilsProjectToDoList

--- src/main/java/com/fsouoliveira/todolist/task/TaskController.java
+++ src/main/java/com/fsouoliveira/todolist/task/TaskController.java
@@ -1,5 +1,6 @@
 package com.fsouoliveira.todolist.task;
 
+import com.fsouoliveira.todolist.utils.Utils;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -18,46 +19,72 @@ public class TaskController {
 
     @PostMapping("/")
     public ResponseEntity<?> create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
-        var idUser = request.getAttribute("idUser");
-        if (idUser == null) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("User ID not found.");
-        }
-        taskModel.setIdUser((UUID) idUser);
+        try {
+            UUID idUser = (UUID) request.getAttribute("idUser");
+            if (idUser == null) {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("User ID not found.");
+            }
+            taskModel.setIdUser(idUser);
 
-        if (taskModel.getStartAt() == null || taskModel.getEndAT() == null) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Start and end dates are required.");
-        }
+            if (taskModel.getStartAt() == null || taskModel.getEndAT() == null) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Start and end dates are required.");
+            }
 
-        var currentDate = LocalDateTime.now();
-        if (currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAT())) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("The start / termination date must be in the future.");
-        }
+            var currentDate = LocalDateTime.now();
+            if (currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAT())) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Dates must be in the future.");
+            }
 
-        if (taskModel.getStartAt().isAfter(taskModel.getEndAT())) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("The start date must precede the end date.");
-        }
+            if (taskModel.getStartAt().isAfter(taskModel.getEndAT())) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("The start date must precede the end date.");
+            }
 
-        var task = this.taskRepository.save(taskModel);
-        return ResponseEntity.status(HttpStatus.OK).body(task);
+            var task = this.taskRepository.save(taskModel);
+            return ResponseEntity.status(HttpStatus.CREATED).body(task);
+        } catch (ClassCastException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid user ID format.");
+        }
     }
 
     @GetMapping("/")
     public ResponseEntity<?> list(HttpServletRequest request) {
-        var idUser = request.getAttribute("idUser");
-        if (idUser == null) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("User ID not found.");
-        }
+        try {
+            UUID idUser = (UUID) request.getAttribute("idUser");
+            if (idUser == null) {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("User ID not found.");
+            }
 
-        var tasks = this.taskRepository.findByIdUser((UUID) idUser);
-        return ResponseEntity.ok(tasks);
+            var tasks = this.taskRepository.findByIdUser(idUser);
+            return ResponseEntity.ok(tasks);
+        } catch (ClassCastException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid user ID format.");
+        }
     }
 
     @PutMapping("/{id}")
-    public TaskModel update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
+    public ResponseEntity<?> update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
+        try {
+            UUID idUser = (UUID) request.getAttribute("idUser");
+            if (idUser == null) {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("User ID not found.");
+            }
 
-        var idUser = request.getAttribute("idUser");
-        taskModel.setIdUser((UUID) idUser);
-        taskModel.setId(id);
-        return this.taskRepository.save(taskModel);
+            var taskOptional = this.taskRepository.findById(id);
+            if (taskOptional.isEmpty()) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Task not found.");
+            }
+
+            var task = taskOptional.get();
+            if (!task.getIdUser().equals(idUser)) {
+                return ResponseEntity.status(HttpStatus.FORBIDDEN).body("User without permission to change this task.");
+            }
+
+            Utils.copyNonNullProperties(taskModel, task);
+            var taskUpdate = this.taskRepository.save(task);
+            return ResponseEntity.ok(taskUpdate);
+        } catch (ClassCastException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid user ID format.");
+        }
     }
 }
+
